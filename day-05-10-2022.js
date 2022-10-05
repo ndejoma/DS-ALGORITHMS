d00327e281a86bbf34d200ec50adc2ee78aaf829
404Xavier <wakirithomaitho100@protonmai.com>
2022-10-05 14:11:10 +0300

    Sum zero refactor which is more efficient

--- day-05-10-2022.js
+++ day-05-10-2022.js
@@ -9,14 +9,43 @@
 export const pairWithSumZero = arr => {
 	for (let i = 0; i < arr.length; i++) {
 		for (let j = i + 1; j < arr.length; j++) {
-            console.log( `i is ${ i } and j is ${ j }` );
-            //check if two numbers sum to zero when the number is not itself to prevent the case with num zero
-			if (arr[i] + arr[j] === 0 ) {
+			console.log(`i is ${i} and j is ${j}`);
+			//check if two numbers sum to zero when the number is not itself to prevent the case with num zero
+			if (arr[i] + arr[j] === 0) {
 				console.log(
 					`Sum is zero when i is ${i} and the value ${arr[j]} and j is ${j} and the value is ${arr[j]}`
 				);
 				return [arr[i], arr[j]];
 			}
+		}
+	}
+};
+
+export const pairWithSumZeroRefactor = arr => {
+	//cannot find a pair in an array with one element or empty
+	if (arr.length <= 1) {
+		return;
+	}
+	let leftIdx = 0;
+	let rightIdx = arr.length - 1;
+
+	while (leftIdx < rightIdx) {
+		let sum = arr[leftIdx] + arr[rightIdx];
+
+		//if the sum is zero return
+		if (sum === 0) {
+			return [arr[leftIdx], arr[rightIdx]];
+		} else if (sum > 0) {
+			/**8
+			 * If the sum is greater than zero it means the array element which might be  pair might be present so decerement the rightIdx value to look up array with indices less than it
+			 * WHere we are likelt to get the pair of the leftIdx
+			 * Otherwsie if the sum is less than zero it means that number does not have a pair so
+			 * increment the leftIdx as we like to get its pair
+			 */
+			rightIdx--;
+		} else {
+			//if the sum is less than zero increment the leftIdx
+			leftIdx++;
 		}
 	}
 };
--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 /** @format */
-import { pairWithSumZero } from './day-05-10-2022.js';
+import { pairWithSumZero, pairWithSumZeroRefactor } from './day-05-10-2022.js';
 
 const firstPair = pairWithSumZero([0, 1, 2, 3]);
 console.log(firstPair); //[-3, 3]
+
+const value = pairWithSumZeroRefactor([-3, -2, 0, 3, 4, 5]);
+console.log(value); //[-3, 3]
