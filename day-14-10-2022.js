8ed9fc1187e73ed2631a2621b61f4464d2c25149
404Xavier <wakirithomaitho100@protonmai.com>
2022-10-14 13:12:37 +0300

    Power recursion example

---
+++ day-14-10-2022.js
@@ -0,0 +1,50 @@
+/**
+ * Recursion challenges
+ *
+ * @format
+ */
+
+/***
+ * Challenge One
+ * POWER
+ *  power(base, exponent)
+power(2,4) //16
+power(3,2) //9
+power(3,3) //27
+Write a function which accepts a base an an exponent.  It should return the result of raising the base to that exponent.
+ */
+
+/***
+ * In this case we have power(3, 4)
+ *  1. THe first call have the base as 3 and the exponent as 4
+ *     SO we execute the return 3 * power(3, 4 -1 ) in which the function calls itself with power(3, 3)
+ *  2. In the second function call the base is 3 and the exponent is 3
+ *     So we execute the return 3 * power(3, 3 - 1) in which it calls itself with power(3, 2)
+ * 
+ *  3. In the third function call the base will be 3 and the exponent will be 2
+ *   So we execute the return 3 * power(3, 2- 1) in which it calls itself with power(3, 1)
+ *  
+ *  4. In the fourth function is pushed on top of the call stack with base 3 and exponent of 1
+ *   SO we execute the return 3 * power(3, 1 - 1) and the function calls itself once more
+ * 
+ *  5. In the fifth function call calls the power(3, 0) is pushed on top of the stack and invoked
+ *   SInce the exponent is 0 we have hit the base case so we return 1 from the power(3, 0) function call
+ * 
+ *  6. The power(3, 0) is popped of the call stack once it returns 1
+ *  
+ *  THe other function power(3, 1) returns 3 * 1  = 3
+ *  The function which called power(3,1) is the power(3, 2) function returns 3 * 3 = 9
+ *  The function which called power(3, 2) is the power(3, 3) function returns 3 * 9 = 27
+ *  The function which called power(3, 3) is the power(3, 4) return 3 * 27 = 81
+ *  Once the function power(3,4) returns 81 is popped off the stack and now the value is 81
+ *  
+ */
+
+export const power = (base, exponent) => {
+	//the base case
+	if (exponent === 0) {
+		return 1;
+	}
+
+	return base * power(base, exponent - 1);
+};
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /** @format */
-import { minSubArrayLen } from './day-13-10-2022.js';
+import { power } from './day-14-10-2022.js';
 
-const value = minSubArrayLen([2, 3, 1, 2, 4, 3], 7);
+const value = power(3, 4);
 console.log(value);
