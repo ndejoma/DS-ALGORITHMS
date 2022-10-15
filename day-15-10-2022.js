5574091dd692f1376dbd9ef6ad946dd287a57c51
404Xavier <wakirithomaitho100@protonmai.com>
2022-10-15 14:02:25 +0300

    String reversal using recursion

--- day-15-10-2022.js
+++ day-15-10-2022.js
@@ -23,6 +23,36 @@ export const recursiveRange = num => {
 	if (num === 0) {
 		return num;
 	}
-
 	return num + recursiveRange(num - 1);
+};
+
+/***
+ * Write a recursive function called reverse which accepts a string and returns a new string in reverse.
+ * reverseString('javascript') we get tpircsavaj
+ */
+/***
+ * This function simply does this for example: john
+ *  We take john as our initial input to get the reversed str using iteration
+ *  We take the last char in the str by slicing it from the last index eg 'john'.slice(3) which will be  n
+ *  Add the end to the reverseString('john'.slice(0, 'john'.length)) which cases a recursion with reverseString('joh')
+ *   The  function calls itself  with reversString('joh') this return  hoj
+ *   THe function calls itself again with reverseString('jo') //this returns oj
+ *   The function calls itself again with reverseStr('j') //this returns j
+ *   By this point we have reached the base case so the function returns j
+ *   The
+ *   Once all the recursive function return  the reverseString('john') returns n + hoj = nhoj
+ *   THis returns the string nhoj and the function is popped off the stack and the valueThree get the value 'nhoj'
+ */
+export const reverseString = str => {
+	if (!str) {
+		throw new Error('Cannot reverse a empty string');
+	}
+
+	if (str.length === 1) {
+		return str;
+	}
+
+	return (
+		str.slice(str.length - 1) + reverseString(str.slice(0, str.length - 1))
+	);
 };
--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 /** @format */
-import { recursiveRange, iterativeRange } from './day-15-10-2022.js';
+import {
+	recursiveRange,
+	iterativeRange,
+	reverseString
+} from './day-15-10-2022.js';
 
 const valueOne = recursiveRange(10);
 console.log(valueOne);
 const valueTwo = iterativeRange(10);
 console.log(valueTwo);
+
+//string reversal
+const valueThree = reverseString('javascript');
+console.log(valueThree, 'The value');
